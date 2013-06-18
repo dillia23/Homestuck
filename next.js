1af089f682ac01efcee3c6dae076728206dfe21c
dillia23 <dillia23@uw.edu>
2013-06-18 10:04:04 -0600

    Update next.js

--- next.js
+++ next.js
@@ -1,27 +1,22 @@
 //link to the next comic
 // var NEXT_LINK = document.getElementsByTagName('table')[7].children[0].children[0].children[0].children[0].children[0].href;
 // var BACK_LINK = document.getElementsByTagName('table')[7].children[0].children[0].children[0].children[4].children[1].children[0].href"";
-var LINK = 'http://www.mspaintadventures.com/?s=6&p='
-var CURR_PAGE = window.location.search.split('p=')[1];
+var LINK = 'http://www.mspaintadventures.com/?s=6&p=00' 
+var CURR_PAGE = parseInt(window.location.search.split('p=')[1]);
 
 
-//wait for DOM to load
-// Run our kitten generation script as soon as the document's DOM is ready.
-document.addEventListener('DOMContentLoaded', function () {
-	console.log("loaded");
-	var text = createTextNode("Loaded!");
-	document.getElementById('text').appendChild(text);
-	var bg = chrome.extension.getBackgroundPage;
-  bg.onkeydown=function(){
+//wait for window to load
+window.onload = function () {
+  window.onkeydown=function(){
   	var key = String.fromCharCode(event.keyCode);
   	if(key == '\'') {
   		//next
-  		bg.location.href = bg.location.href = LINK + (CURR_PAGE + 1);
+  		window.location.href = LINK + (CURR_PAGE + 1);
   	} else if (key == '%') {
   		//go back
-  		bg.location.href = LINK + (CURR_PAGE - 1);
+  		window.location.href = LINK + (CURR_PAGE - 1);
   	} else {
   		console.log(key);
   	}
   };
-});
+};
